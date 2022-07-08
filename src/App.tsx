367f9541d0d44932d06d8a8a353444dfb83a7a6a
ryota0624 <tennis.suta6@gmail.com>
2022-07-08 14:46:17 +0900

    ページの構造を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { Button, ChakraProvider, Image, Link, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  ChakraProvider,
+  Container, extendTheme, HStack,
+  Image,
+  Link,
+  Text,
+  VStack
+} from '@chakra-ui/react';
 import { RecoilRoot } from 'recoil';
 import './App.css';
 import * as Counter from './counter';
@@ -7,47 +16,98 @@ import logo from './logo.svg';
 function App() {
   const [count, increment] = Counter.useCounter();
   return (
-    <div className="App">
-      <header className="App-header">
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Text>Hello Vite + React!</Text>
-        <Text>
-          <Button type="button" onClick={increment} textColor="gray">
+    <>
+      <Header />
+      <Container
+        className="App"
+        backgroundColor={'blackAlpha.500'}
+        scrollBehavior="smooth"
+      >
+        {' '}
+        <VStack>
+          <Button
+            type="button"
+            onClick={increment}
+            textColor="gray"
+            margin={'1'}
+          >
             count is: {count}
           </Button>
-        </Text>
-        <Text>fibo(coutner) = {Counter.useCounterFibonattiState()}</Text>
-        <Text>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </Text>
-        <Text>
-          <Link color={"lightblue"}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-          {' | '}
-          <Link color={"lightblue"}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </Link>
-        </Text>
-      </header>
-    </div>
+          <Text>fibo(coutner) = {Counter.useCounterFibonattiState()}</Text>
+          <Text>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </Text>
+        </VStack>
+      </Container>
+
+      <Footer></Footer>
+    </>
   );
 }
 
+function Header() {
+  return (
+    <Container border="1px" borderColor={'blue.100'}>
+      <HStack backgroundColor={'white'} h="50px">
+        <Image h="50px" src={logo} className="App-logo" alt="logo" />
+        <Text>Hello Vite + React!</Text>
+      </HStack>
+    </Container>
+  );
+}
+
+function Footer() {
+  return (
+    <Center>
+      <Container
+        centerContent
+        position={'fixed'}
+        bottom="0"
+        backgroundColor={'black'}
+      >
+        <Center h={'50px'}>
+          <Text>
+            <Link
+              color={'lightblue'}
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </Link>
+            {' | '}
+            <Link
+              color={'lightblue'}
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </Link>
+          </Text>
+        </Center>
+      </Container>
+    </Center>
+  );
+}
+
+const theme = extendTheme({
+  components: {
+    Container: {
+      baseStyle: {
+        maxWidth: "100%"
+      }
+    }
+  }
+})
+
 export default function () {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RecoilRoot>
         <App />
       </RecoilRoot>
     </ChakraProvider>
   );
 }
+
